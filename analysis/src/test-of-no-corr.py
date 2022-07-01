c869a905d63a55c713b63eb9fcd038e5a3c3978c
MKryo <ryotab220716@icloud.com>
2022-07-01 15:42:37 +0900

    p値出力形式変更

--- analysis/src/test-of-no-corr.py
+++ analysis/src/test-of-no-corr.py
@@ -1,5 +1,6 @@
 from re import A
 from sys import api_version
+import csv
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -31,6 +32,7 @@ df_original = pd.read_csv('../data/model-to-data/original.csv')
 df_Faithful = pd.read_csv('../data/model-to-data/Faithful.csv')
 df_Change = pd.read_csv('../data/model-to-data/Change.csv')
 
+
 # LS00オリジナルの実験データ
 # 無相関検定
 mean = df_original['mean'].values
@@ -39,7 +41,10 @@ paris = df_original['pARIs'].values
 deltap = df_original['ΔP'].values
 #ピアソンの相関係数とp値を計算
 p_values = [pearsonr(mean, dfh), pearsonr(mean, paris), pearsonr(mean, deltap)]
-print('(相関係数, p値):', p_values)
+# print('(相関係数, p値):', p_values)
+for i in range(len(p_values)):
+    print(p_values[i][1])
+
 
 # LS00忠実再現実験データ
 # 無相関検定
@@ -49,7 +54,10 @@ paris = df_Faithful['pARIs'].values
 deltap = df_Faithful['ΔP'].values
 #ピアソンの相関係数とp値を計算
 p_values = [pearsonr(mean, dfh), pearsonr(mean, paris), pearsonr(mean, deltap)]
-print('(相関係数, p値):', p_values)
+# print('(相関係数, p値):', p_values)
+for i in range(len(p_values)):
+    print(p_values[i][1])
+
 
 # LS00変更実験データ
 # 無相関検定
@@ -59,8 +67,16 @@ paris = df_Change['pARIs'].values
 deltap = df_Change['ΔP'].values
 #ピアソンの相関係数とp値を計算
 p_values = [pearsonr(mean, dfh), pearsonr(mean, paris), pearsonr(mean, deltap)]
-print('(相関係数, p値):', p_values)
+# print('(相関係数, p値):', p_values)
+for i in range(len(p_values)):
+    print(p_values[i][1])
 
+
+
+# with open('../data/out/p_value.csv', 'w') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(p_values)
+    
 
 #### 結果 #####
 # 全ての相関係数において p値が0.05より小さいので無相関検定において統計的に有意
